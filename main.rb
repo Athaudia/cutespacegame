39372751939ec8683d12b6e44bad22a602e98b97
Athaudia <athaudia@gmail.com>
2011-09-12 03:26:38 +0200

    Added InfoScreen, closes #4

--- main.rb
+++ main.rb
@@ -660,7 +660,7 @@ class Game < Chingu::GameState
 		self.viewport.game_area = [0,0,3000,3000]
 		puts "Took #{Time.now-$startuptime}s to start up"
 		@spawntimer = 800
-		@spawntime = 0
+		@spawntime = 1
 	end
 
 	def draw
@@ -694,7 +694,7 @@ class Game < Chingu::GameState
 
 	def update
 		super
-#		push_game_state ShopState.new(@player)
+		if not @notfirst then push_game_state InfoScreen; @notfirst = true; end
 
 		self.viewport.center_around @player
 
@@ -737,6 +737,35 @@ class Game < Chingu::GameState
 		end
 
 		$tick += 1
+	end
+end
+
+class InfoScreen < Chingu::GameState
+	def initialize
+		super
+		Chingu::Text.create("Athaudia's\nCute Space Game", :x => 10, :y => 10, :factor_x => 2, :factor_y => 2)
+		Chingu::Text.create("version: #{VERSION}", :x => 5, :y => 588-5)
+		text = """Keys:
+
+  A, D: turn
+  W, S: accelerate, decelerate
+  shift-A, shift-D, Q, E: strafe
+  H,J,K,L: fire weapons
+
+Goal:
+
+  Shoot enemies, earn money and points,
+  spend money in one of the 4 upgrade shops
+  at the corners of the map, shoot more enemies.
+
+[space to start]"""
+		Chingu::Text.create(text, :x => 10, :y => 100)
+		self.input = {:space => :pop_game_state}
+	end
+
+	def draw
+		fill_rect [0,0,800,600], 0xff000030, -2
+		super
 	end
 end
 
