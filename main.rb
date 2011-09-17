84566593e82503370d1a367b63ba3d65f3a987fc
Athaudia <athaudia@gmail.com>
2011-09-17 16:27:34 +0200

    AI leading shots better now

--- main.rb
+++ main.rb
@@ -174,6 +174,7 @@ class PlayerShip < Ship
 		@keys = @type[:slots].map{[true,false,false,false]} unless options[:ghost]
 		@crosshair = Chingu::GameObject.create(:image => "crosshair.png", :scale => 2, :zorder => 9000) unless options[:ghost]
 		@ghost = PlayerShip.new(ghost: true, type: @type) unless options[:ghost]
+		@rsstack = []
 	end
 
 	def upgrade(type)
@@ -192,10 +193,10 @@ class PlayerShip < Ship
 		end
 		super
 		$game.viewport.center_around self if self == $player
-		if $config[:control] == :mouse
+		if $config[:control] == :mouse and self == $player
 			@crosshair.x = $window.mouse_x + $game.viewport.x if self == $player
 			@crosshair.y = $window.mouse_y + $game.viewport.y if self == $player
-			a = Math.atan2(-$player.crosshair.x+@x,$player.crosshair.y-@y)*180.to_f/Math::PI+180
+			a = Math.atan2(-$player.crosshair.x+@x,$player.crosshair.y-@y)*180.to_f/Math::PI+180 if self == $player
 			diff = a - self.angle;
 			diff += 360 while diff < -180
 			diff -= 360 while diff > 180
@@ -207,19 +208,16 @@ class PlayerShip < Ship
 			@crosshair.y = @y - Math.cos(self.angle.to_radian) * 50 if self == $player
 		end
 		if self == $player
-			@ghost.x, @ghost.y, @ghost.angle, @ghost.rs = @x, @y, @angle, @rs
+			nrs = 0
+			nrs = @rsstack.inject(:+)/@rsstack.size if @rsstack.size > 0
+			@ghost.x, @ghost.y, @ghost.angle, @ghost.rs = @x, @y, @angle, nrs
 			@ghost.velocity_x, @ghost.velocity_y = @velocity_x, @velocity_y
-#			puts @ghost.x
-			40.times{@ghost.update}
-#			puts @ghost.x
-			if (@velocity_x.abs+@velocity_y.abs) > 1
-	 			@future_x = @ghost.x
-				@future_y = @ghost.y
-			else
-				@future_x = @x
-				@future_y = @y
-			end
+			40.times{@ghost.update; @ghost.rs = nrs}
+ 			@future_x = @ghost.x
+			@future_y = @ghost.y
 			@crosshair.angle = @angle
+			if @rsstack.size > 100 then @rsstack.shift end
+			@rsstack << @rs
 		end
 	end
 
