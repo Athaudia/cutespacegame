7a5a2b048ad26c2d86643c13225cff3b51d6843e
Athaudia <athaudia@gmail.com>
2011-09-22 16:14:56 +0200

    added hiscores to info screen

--- src/info_screen.rb
+++ src/info_screen.rb
@@ -1,7 +1,8 @@
 class InfoScreen < Chingu::GameState
 	def initialize
 		super
-		Chingu::Text.create("Athaudia's\nCute Space Game", :x => 10, :y => 10, :factor_x => 2, :factor_y => 2)
+		Chingu::Text.create("Athaudia's", :x => 10, :y => 10, :factor_x => 2, :factor_y => 2).center
+		Chingu::Text.create("Cute Space Game", :x => 10, :y => 50, :factor_x => 2, :factor_y => 2).center
 		Chingu::Text.create("version: #{VERSION}", :x => 5, :y => 588-5)
 		text = """Keys:
 
@@ -18,11 +19,21 @@ Goal:
 
 [space to start]"""
 		Chingu::Text.create(text, :x => 10, :y => 100)
+		t = Chingu::Text.create("Hiscores", :x => 10, :y => 100, :scale => 1)
+		t.x = (400-t.gosu_font.text_width(t.text, 1))/2 + 400
+		hiscores = Scores::get
+		hiscores = (hiscores*10)[0,10]
+		hiscores.each_with_index do |h,i|
+			t = Chingu::Text.create("#{(i+1)}. #{h['name']} - #{h['score']}", :x => 10, :y => 130+i*15, :scale => 1)
+			t.x = (400-t.gosu_font.text_width(t.text, 1))/2 + 400
+		end
+
 		self.input = {:space => :pop_game_state}
 	end
 
 	def draw
 		fill_rect [0,0,800,600], 0xff000030, -2
+		draw_rect [410,90,380,200], 0xffffffff, -1
 		super
 	end
 end
